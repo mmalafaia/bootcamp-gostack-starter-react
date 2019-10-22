f21369471e3a36b8addf441e23479f16c49fa4c3
mmalafaia <mmalafaia@gmail.com>
2019-10-22 14:49:02 -0300

    Commit: Minimal Corrections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import TechList from './components/TechList'
+import TechList from './components/TechList';
 
 function App() {
   return <TechList />
